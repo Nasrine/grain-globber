70bbfda746e8a7f5d45fd1b1c47b188b06fc4463
Nasrine <azar0006@algonquinlive.com>
2016-03-16 12:32:48 -0400

    Magic- put "/ "after images to see the images

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ var $ul = $('ul');
 
 $ul.addClass('grains');
 
-grains.forEach(function (grain){
+grains.forEach(function (grain) {
   var $li = $('<li>');
   var $h2 = $('<h2>');
   var $img = $('<img>');
   var $p = $('<p>');
 
   $h2.html(grain.name);
-  $img.attr('src', 'images' + grain.img);
+  $img.attr('src', 'images/' + grain.img);
   $p.html(grain.desc);
 
   $ul.append($li);
