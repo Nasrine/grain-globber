ee558a33d7f9eef0be9d3756df63f37500ff3d14
Nasrine <azar0006@algonquinlive.com>
2016-03-16 12:13:33 -0400

    display block for css grains li

--- js/main.js
+++ js/main.js
@@ -1,13 +1,8 @@
-var $h1 = $('h1');
-
-$h1.html('Grain globber');
-$ul.addClass ('grains-list');
 var $ul = $('ul');
 
-$h1.html('Grain globber!');
 $ul.addClass('grains');
 
-dinos.forEach(function (grain){
+grains.forEach(function (grain){
   var $li = $('<li>');
   var $h2 = $('<h2>');
   var $img = $('<img>');
@@ -20,5 +15,4 @@ dinos.forEach(function (grain){
   $ul.append($li);
   $li.append($h2, $img, $p);
   $p.append($p);
-}
-  );
+});
